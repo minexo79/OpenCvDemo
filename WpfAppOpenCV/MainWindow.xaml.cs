a5e9be18e27181290b6c35fa8285d18ea039e11c
minexo79 <minexo79@gmail.com>
2023-08-25 01:20:06 +0800

    initial commit

---
+++ WpfAppOpenCV/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Window = System.Windows.Window;
+
+namespace WpfAppOpenCV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string client = "";
+
+        rtspCamera camera;
+        bool isSaving = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            camera = new rtspCamera();
+            camera.OnFrameReceived += Camera_OnFrameReceived;
+        }
+
+        private void Camera_OnFrameReceived(object? sender, EventArgs e)
+        {
+            image.Dispatcher.Invoke(delegate 
+            { 
+                image.Source = ((FrameArgs)e).bmp; 
+            });
+
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            client = $"rtsp://{tbxIp.Text}:{tbxPort.Text}/avix1";
+
+            camera.Start(client);
+        }
+
+        private void btnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            camera.Stop();
+        }
+    }
+}
---
+++ WpfAppOpenCV/rtspCamera.cs
@@ -0,0 +1,104 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WpfAppOpenCV
+{
+    public class FrameArgs : EventArgs
+    {
+        public BitmapSource bmp;
+    }
+
+    public class rtspCamera
+    {
+        bool isProcessing = false;
+
+        private static object locker = new object();
+        public event EventHandler OnFrameReceived;
+
+        private Mat frameCache;
+        FrameArgs args;
+        VideoCapture capture;
+        CancellationTokenSource cts, cts2;
+
+        public rtspCamera()
+        {
+            args = new FrameArgs();
+            capture = new VideoCapture();
+        }
+
+        public void Start(string _url)
+        {
+            cts = new CancellationTokenSource();
+            cts2 = new CancellationTokenSource();
+
+            capture.Open(_url, VideoCaptureAPIs.ANY);
+
+            Task task = new Task(() => 
+            {
+                while(true)
+                {
+                    if (cts.IsCancellationRequested)
+                    {
+                        capture.Release();
+                        return;
+                    }
+
+                    lock (locker)
+                    {
+                        isProcessing = true;
+
+                        using (Mat frame = new Mat())
+                        {
+                            bool success = capture.Read(frame);
+                            if (frame.Empty()) continue;
+                            frameCache = frame.Clone();
+                        }
+
+                        isProcessing = false;
+                    }
+                }
+            }, cts.Token);
+
+            Task task2 = new Task(() => 
+            {
+                while (true)
+                {
+                    if (cts2.IsCancellationRequested)
+                    {
+                        return;
+                    }
+
+                    if (!isProcessing && frameCache != null)
+                    {
+                        using (Mat frame = frameCache.Clone())
+                        {
+                            args.bmp = frame.ToBitmapSource();
+                            OnFrameReceived(this, args);
+                        }
+                    }
+                }
+            }, cts2.Token);
+
+            task.Start();
+            task2.Start();
+        }
+
+        public void Stop()
+        {
+            cts2?.Cancel();
+            cts2?.Dispose();
+
+            cts?.Cancel();
+            cts?.Dispose();
+        }
+    }
+}
